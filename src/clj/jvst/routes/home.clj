91d71f4c79b57d4b938b670b2e44d4941f07bb25
Tevis Tsai <SystemicError@gmail.com>
2019-01-17 16:54:54 +0000

    Added some test code so homepage shows session data.

--- src/clj/jvst/routes/home.clj
+++ src/clj/jvst/routes/home.clj
@@ -3,11 +3,52 @@
             [jvst.db.core :as db]
             [compojure.core :refer [defroutes GET]]
             [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+            [clojure.java.io :as io]
+            [buddy.hashers :as hashers]
+            [clojure.tools.logging :as log]
+            [clojure.string :as str]))
 
-(defn home-page []
+
+;;; adapted code
+
+;;; "DATABASE"
+
+(defn get-user [email]
+  (db/get-user {:email email}))
+
+(defn add-user! [email password first-name last-name]
+  (if (get-user (str/lower-case email))
+    (throw (Exception. "User with that email already exists!"))
+    (db/create-user! {:email (str/lower-case email)
+                      :password password
+                      :first-name first-name
+                      :last-name last-name})))
+
+;;; PASSWORD
+(defn password-hash
+  [password]
+  (hashers/derive password {:alg :bcrypt+sha512}))
+
+(defn validate-password [email password]
+  (hashers/check password (get (get-user email) :password)))
+
+
+;;; USER FUNCTIONALITY
+
+(defn add-user-to-session [request user]
+  (assoc-in request [:session :identity] user)) ; http://www.luminusweb.net/docs/routes.html#restricting_access
+
+(defn clear-session-identity [request]
+  (assoc-in request [:session :identity] nil))
+
+
+;;;
+
+
+(defn home-page [request]
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"
+    {:message request}))
 
 (defn test-page []
   (layout/render
@@ -17,7 +58,6 @@
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
+  (GET "/" request (home-page request))
   (GET "/test" [] (test-page))
   (GET "/about" [] (about-page)))
-
