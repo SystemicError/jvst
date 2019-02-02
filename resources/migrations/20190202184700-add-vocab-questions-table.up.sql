7557c22fdddcb72cb87cb8e709dbe6b3857b871f
Tevis Tsai <SystemicError@gmail.com>
2019-02-02 23:48:33 +0000

    Added migrations for new vocab-questions table.

---
+++ resources/migrations/20190202184700-add-vocab-questions-table.up.sql
@@ -0,0 +1,11 @@
+CREATE TABLE vocab_questions
+(id INTEGER PRIMARY KEY,
+ set INTEGER,
+ headword VARCHAR(30),
+ furigana VARCHAR(30),
+ example VARCHAR(300),
+ correct VARCHAR(30),
+ option_1 VARCHAR(30),
+ option_2 VARCHAR(30),
+ option_3 VARCHAR(30),
+ option_4 VARCHAR(30));
